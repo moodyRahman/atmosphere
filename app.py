808c209cae2afe18298a4a2e2642e6dc5f6b6713
Moody Rahman <mrahman01@stuy.edu>
2019-11-19 09:21:37 -0500

    beautified + session logics

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-#Joseph Yusufov, Mudadour Rahman, Alice Ni, David Wang
-#SoftDev1 pd2
-#Atmosphere
-#2019-11-xx
+# Joseph Yusufov, Mudadour Rahman, Alice Ni, David Wang
+# SoftDev1 pd2
+# Atmosphere
+# 2019-11-xx
 
 from flask import Flask, render_template, request, session
 from flask import render_template
@@ -20,14 +20,13 @@ import os
 # m49 = {}
 # reader = csv.reader(open("./data/M49.csv", "r"))
 # for row in reader:
-    # print(row)
-    # m49[row[0]] = row[1]
+# print(row)
+# m49[row[0]] = row[1]
 
 # print(m49)
 
 app = Flask(__name__)  # create instance of class Flask
 app.secret_key = os.urandom(24)
-
 
 
 def runsqlcommand(command):
@@ -38,9 +37,7 @@ def runsqlcommand(command):
     if "select" in command.lower():
         return c.fetchall()
     db.commit()  # save changes
-	db.close()  # close database
-
-
+    db.close()  # close database
 
 
 @app.route("/")  # assign following fxn to run when root route requested
@@ -48,15 +45,19 @@ def index():
     return render_template('index.html')
 
 
-
 @app.route("/login")
 def login():
-	if "username" in request.args:
-		# sqlite check
-	else:
-		return render_template("login.html")
-	return "reee"
-
+    if "username" in request.args:
+        command = "SELECT * FROM loginfo"
+        pair = runsqlcommand(command)
+        if len(pair) == 0:
+            return "WRONG USERNAME"
+        else:
+            session["username"] = request.args[pair["username"]]
+            return (session["username"] + " is logged in")
+    else:
+        return render_template("login.html")
+    return "reee"
 
 
 @app.route("/lookup")
@@ -64,7 +65,7 @@ def lookup():
     if 'user' in session:
         if request.args:
             r = urllib.request.urlopen(
-                "" # Some API link goes here
+                ""  # Some API link goes here
             )
             data = json.loads(r.read())
             data = data['data'][0]
@@ -74,15 +75,13 @@ def lookup():
 
         else:
             r = urllib.request.urlopen(
-                "" # Some API link goes here
+                ""  # Some API link goes here
             )
             data = json.loads(r.read())
             data = data['data'][0]
-            print(data);
+            print(data)
             # print(data['results'][0]['name'])
             return render_template("lookup.html", data=data)
-
-
 
 
 if __name__ == "__main__":
